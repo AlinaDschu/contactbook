a8f0a027696eb31e8353eb615e4bd24631c48ca0
Пользователь <tatetatetate2201@gmail.com>
2022-03-17 12:57:29 +0600

    first commit

---
+++ src/components/AddContact/AddContact.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+
+const AddContact = (props) => {
+  let [name, setName] = useState("");
+  let [surname, setSurname] = useState("");
+  let [phone, setPhone] = useState("");
+
+  function handleClick() {
+    let newContact = {
+      name,
+      surname,
+      phone,
+      id: Date.now(),
+    };
+    props.handleNewContact(newContact);
+    setName("");
+    setSurname("");
+    setPhone("");
+  }
+
+  return (
+    <div>
+      <input
+        onChange={(e) => setName(e.target.value)}
+        type="text"
+        placeholder="Имя"
+        value={name}
+      ></input>
+      <input
+        onChange={(e) => setSurname(e.target.value)}
+        type="text"
+        placeholder="Фамилия"
+        value={surname}
+      ></input>
+      <input
+        onChange={(e) => setPhone(e.target.value)}
+        type="text"
+        placeholder="Номер телефона"
+        value={phone}
+      ></input>
+      <button onClick={handleClick}>Add Contact</button>
+    </div>
+  );
+};
+// contacts: [{ name: "Kubat", surname: "Abdrakhmanov", phone: +79106699888 }];
+
+export default AddContact;
